28df8398c7f7c479b3885a854cf333e6efe803de
Jared Torii <jtorii2@zagmail.gonzaga.edu>
2021-12-01 20:16:11 -0800

    xml files

---
+++ app/src/main/java/com/example/cpsc312project/JoinScreen.java
@@ -0,0 +1,14 @@
+package com.example.cpsc312project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JoinScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_screen);
+    }
+}
